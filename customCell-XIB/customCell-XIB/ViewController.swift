643aedc375f0dbe6f48f79ff61be8586269b6f68
Alysson M <almenezes912@gmail.com>
2022-10-23 14:23:14 -0300

    criando projeto inicial

---
+++ customCell-XIB/customCell-XIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customCell-XIB
+//
+//  Created by ALYSSON MENEZES on 23/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
